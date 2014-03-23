c098899696b1ea198bbfcb73744ec0b613f91005
Filippo Squillace <squillace.filippo@gmail.com>
2014-03-23 19:37:26 +0000

    Add a mini prompt for ssh mini pearl

--- lib/ssh.sh
+++ lib/ssh.sh
@@ -9,6 +9,8 @@ function ssh_mini_pearl(){
 local homeScript=""
 [ -f $PEARL_HOME/pearlsshrc ] && homeScript=$(cat $PEARL_HOME/pearlsshrc)
 
+local promptScript="export PS1=\"\[\033[0m\]\[\033[31m\][\[\033[32m\]\T\[\033[0m\] \[\033[38;5;108m\]\u\[\033[31m\]@\[\033[0m\]\[\033[38;5;112m\]\h \W \[\033[35m\]\$\[\033[31m\]]>\[\033[0m\] \""
+
 local aliasesScript=""
 local opsScript=""
 if [ -d "$PEARL_ROOT" ];
@@ -16,8 +18,9 @@ then
     aliasesScript=$(cat $PEARL_ROOT/lib/aliases.sh)
     opsScript="$(cat $PEARL_ROOT/lib/ops.sh)"
 fi
+local fromPearlScript=$(echo "$aliasesScript\n$opsScript\n")
 
-local commandScript=$(echo "$aliasesScript\n$opsScript\n$homeScript")
+local commandScript=$(echo "$fromPearlScript\n$promptScript\n$homeScript")
 commandScript=$(hexencode "$commandScript")
 
 CMD="commandScript=\"${commandScript}\"; PEARL_INSTALL=\$(mktemp -d pearl-XXXXX -p /tmp); echo \"\$(printf '%b' \"\${commandScript}\")\" > \${PEARL_INSTALL}/minipearl.sh; bash --rcfile \${PEARL_INSTALL}/minipearl.sh -i; [ -d \${PEARL_INSTALL} ] && rm -rf \${PEARL_INSTALL}"
