162a334d62347e8f1f19c9066eb1d02fc816bf52
Filippo Squillace <squillace.filippo@gmail.com>
2014-04-06 14:34:37 +0100

    Move lib/ssh.sh in pearl-ssh module

--- lib/mods.sh
+++ lib/mods.sh
@@ -1,4 +1,11 @@
 
+function pearl_ssh_config(){
+    MANPATH=$MANPATH:$PEARL_ROOT/mods/pearl-ssh/man
+    PEARL_SSH_ROOT="${PEARL_ROOT}/mods/pearl-ssh"
+    source ${PEARL_ROOT}/mods/pearl-ssh/lib/ssh.sh
+    return 0
+}
+
 function pearl_man_config(){
     PATH=$PATH:$PEARL_ROOT/mods/pearl-man/bin
     MANPATH=$MANPATH:$PEARL_ROOT/mods/pearl-man/man
@@ -12,8 +19,10 @@ function powerline_config(){
 }
 
 [ "$(ls -A "${PEARL_ROOT}/mods/pearl-man")" ] && pearl_man_config
+[ "$(ls -A "${PEARL_ROOT}/mods/pearl-ssh")" ] && pearl_ssh_config
 [ "$(ls -A "${PEARL_ROOT}/mods/powerline")" ] && powerline_config
 
 
 unset pearl_man_config
+unset pearl_ssh_config
 unset powerline_config
