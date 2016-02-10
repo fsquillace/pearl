557de811c5da57d03b4cb0732ae8131d15d6feeb
Filippo Squillace <squillace.filippo@gmail.com>
2016-02-10 19:20:05 +0000

    Update p4merge

--- lib/core/mods/pearl/p4merge/install.sh
+++ lib/core/mods/pearl/p4merge/install.sh
@@ -1,8 +1,10 @@
 
 function post_install(){
     ${PEARL_ROOT}/mods/pearl/p4merge/bin/p4install
+    apply "[include] path = \"$PEARL_ROOT/mods/pearl/p4merge/etc/gitconfig\"" "$HOME/.gitconfig"
 }
 
 function pre_uninstall(){
     ${PEARL_ROOT}/mods/pearl/p4merge/bin/p4uninstall
+    unapply "[include] path = \"$PEARL_ROOT/mods/pearl/p4merge/etc/gitconfig\"" "$HOME/.gitconfig"
 }
