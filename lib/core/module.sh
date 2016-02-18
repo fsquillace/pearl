1be8a1531d96d423ee618acec809cb02c104ef8a
Filippo Squillace <squillace.filippo@gmail.com>
2016-02-18 22:45:14 +0000

    Add description to the pearl modules

--- lib/core/module.sh
+++ lib/core/module.sh
@@ -1,6 +1,35 @@
 # This module handles the pearl modules
 #
 
+declare -A descriptions
+descriptions=( \
+    ["misc/liquidprompt"]="A full-featured adaptive prompt (https://github.com/nojhan/liquidprompt) [bash][zsh]" \
+    ["misc/ls-colors"]="A collection of LS_COLORS definitions (https://github.com/trapd00r/LS_COLORS) [bash][zsh][fish]" \
+    ["misc/powerline"]="Powerline is a statusline plugin for vim, zsh, bash, tmux (https://github.com/powerline/powerline)" \
+    ["misc/powerline-fonts"]="Patched fonts for Powerline (https://github.com/powerline/fonts)" \
+    ["misc/ranger"]="A VIM-inspired filemanager for the console (http://ranger.nongnu.org/)" \
+    ["pearl/dotfiles"]="The Pearl dotfiles (https://github.com/fsquillace/pearl-dotfiles)" \
+    ["pearl/man"]="Pearl module for snippets from the open source world (https://github.com/fsquillace/pearl-man)" \
+    ["pearl/p4merge"]="P4Merge tool for Pearl (https://github.com/fsquillace/pearl-p4merge)" \
+    ["pearl/ssh"]="20 lines script that brings dotfiles in a ssh session (https://github.com/fsquillace/pearl-ssh)" \
+    ["pearl/utils"]="The Pearl utility functions, aliases etc (https://github.com/fsquillace/pearl-utils)" \
+    ["vim/airline"]="Status/tabline for vim (https://github.com/vim-airline/vim-airline)" \
+    ["vim/bufexplorer"]="BufExplorer Plugin for Vim (https://github.com/jlanzarotta/bufexplorer)" \
+    ["vim/fugitive"]="A Git wrapper so awesome, it should be illegal (https://github.com/tpope/vim-fugitive)" \
+    ["vim/gitgutter"]="Shows a git diff (https://github.com/airblade/vim-gitgutter)" \
+    ["vim/gnupg"]="Editing of gpg encrypted files (https://github.com/jamessan/vim-gnupg)" \
+    ["vim/indentline"]="Display the indention levels with thin vertical lines (https://github.com/Yggdroot/indentLine)" \
+    ["vim/jedi"]="jedi autocompletion library (https://github.com/davidhalter/jedi-vim)" \
+    ["vim/nerdcommenter"]="Intensely orgasmic commenting (https://github.com/scrooloose/nerdcommenter)" \
+    ["vim/nerdtree"]="A tree explorer (https://github.com/scrooloose/nerdtree)" \
+    ["vim/python-mode"]="Vim python-mode (https://github.com/klen/python-mode)" \
+    ["vim/signify"]="Show a diff (https://github.com/mhinz/vim-signify)" \
+    ["vim/solarized"]="Precision colorscheme (https://github.com/altercation/vim-colors-solarized)" \
+    ["vim/supertab"]="Perform all your vim insert mode completions with Tab (https://github.com/ervandew/supertab)" \
+    ["vim/syntastic"]="Syntax checking hacks (https://github.com/scrooloose/syntastic)" \
+    ["vim/zenburn"]="Low-contrast color scheme (https://github.com/jnurmine/Zenburn)" \
+)
+
 function pearl_module_install(){
     local modulename=$1
     local hook_file=${PEARL_ROOT}/lib/core/mods/${modulename}/install.sh
@@ -60,14 +89,18 @@ function pearl_module_uninstall(){
 
 function pearl_module_list(){
     builtin cd $PEARL_ROOT
-    local modlist=$(git submodule status | awk '{print $2}'  | grep -E ^mods | sed 's/mods\///')
+    local modlist=$(git submodule status | grep "^-" | cut -d' ' -f2 | sed -e 's/^mods\///')
     for module in $modlist
     do
-        local installed=""
-        [ "$(ls -A mods/${module})" ] && installed="[installed]"
-        echo "$module $installed"
+        info "$module"
+        echo "    ${descriptions[$module]}"
+    done
+    local modlist=$(git submodule status | grep -v "^-" | cut -d' ' -f3 | sed -e 's/^mods\///')
+    for module in $modlist
+    do
+        info "$module [installed]"
+        echo "    ${descriptions[$module]}"
     done
     builtin cd $OLDPWD
-#    git config --list| grep -E ^submodule
 }
 
