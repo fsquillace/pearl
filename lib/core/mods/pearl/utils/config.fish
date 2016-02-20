497de4d97af452f863e05d513b45fbe53995061b
Filippo Squillace <squillace.filippo@gmail.com>
2016-02-20 17:46:16 +0000

    Issue #11: Update pearl/utils for Fish

---
+++ lib/core/mods/pearl/utils/config.fish
@@ -0,0 +1,5 @@
+
+set MANPATH $MANPATH $PEARL_ROOT/mods/pearl/utils/man
+source $PEARL_ROOT/mods/pearl/utils/lib/aliases.sh
+
+# vim: ft=sh
--- lib/core/mods/pearl/utils/config.sh
+++ lib/core/mods/pearl/utils/config.sh
@@ -3,6 +3,7 @@ MANPATH=$MANPATH:$PEARL_ROOT/mods/pearl/utils/man
 source ${PEARL_ROOT}/mods/pearl/utils/lib/utils.sh
 source ${PEARL_ROOT}/mods/pearl/utils/lib/core.sh
 source ${PEARL_ROOT}/mods/pearl/utils/lib/aliases.sh
+source ${PEARL_ROOT}/mods/pearl/utils/lib/aliases.bash
 source ${PEARL_ROOT}/mods/pearl/utils/lib/ops.sh
 source ${PEARL_ROOT}/mods/pearl/utils/lib/misc.sh
 [ -z "$BASH" ] || source ${PEARL_ROOT}/mods/pearl/utils/lib/prompt.sh
