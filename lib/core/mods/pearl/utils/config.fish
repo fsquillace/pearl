d0bfd7fc0bb047bfcb0e7b6437fb3121e2992388
Filippo Squillace <squillace.filippo@gmail.com>
2016-03-01 22:04:56 +0000

    Issue #11: Add utils and terminals script for Fish

--- lib/core/mods/pearl/utils/config.fish
+++ lib/core/mods/pearl/utils/config.fish
@@ -2,6 +2,8 @@
 set MANPATH $MANPATH $PEARL_ROOT/mods/pearl/utils/man
 set PATH $PATH $PEARL_ROOT/mods/pearl/utils/bin
 source $PEARL_ROOT/mods/pearl/utils/lib/aliases.sh
+source $PEARL_ROOT/mods/pearl/utils/lib/utils.fish
 source $PEARL_ROOT/mods/pearl/utils/lib/prompt.fish
+source $PEARL_ROOT/mods/pearl/utils/lib/terminals.fish
 
 # vim: ft=sh
