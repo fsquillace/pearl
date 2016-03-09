2df72ab95f1384e59886485518d3999b3df3b5f6
Filippo Squillace <squillace.filippo@gmail.com>
2016-03-09 23:03:56 +0000

    Add support for traps in Fish

--- lib/core/mods/pearl/utils/config.fish
+++ lib/core/mods/pearl/utils/config.fish
@@ -6,5 +6,6 @@ source $PEARL_ROOT/mods/pearl/utils/lib/aliases.sh
 source $PEARL_ROOT/mods/pearl/utils/lib/utils.fish
 source $PEARL_ROOT/mods/pearl/utils/lib/prompt.fish
 source $PEARL_ROOT/mods/pearl/utils/lib/terminals.fish
+source $PEARL_ROOT/mods/pearl/utils/lib/traps.fish
 
 # vim: ft=sh
