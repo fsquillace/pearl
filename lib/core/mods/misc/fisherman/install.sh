9f1d26cccaf03cff9957482b6e40e8db43b7d27e
Filippo Squillace <squillace.filippo@gmail.com>
2016-03-18 11:47:14 +0000

    Fix hooks for Fisherman

--- lib/core/mods/misc/fisherman/install.sh
+++ lib/core/mods/misc/fisherman/install.sh
@@ -7,6 +7,9 @@ function post_install(){
 }
 
 function pre_remove(){
-    fish -c "fisher list | fisher uninstall --force"
+    local fish_plugins=$(fish -c "fisher list")
+    [ "$fish_plugins" == "." ] && return
+
+    echo "$fish_plugins" | fish -c "fisher uninstall --force"
     return 0
 }
