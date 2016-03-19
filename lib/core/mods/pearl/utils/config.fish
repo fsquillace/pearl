f6fd9cc309db60d467ad828263a8567ef149894a
Filippo Squillace <squillace.filippo@gmail.com>
2016-03-19 14:36:28 +0000

    Add aliases for Fish on pearl/utils

--- lib/core/mods/pearl/utils/config.fish
+++ lib/core/mods/pearl/utils/config.fish
@@ -3,6 +3,7 @@ set MANPATH $MANPATH $PEARL_ROOT/mods/pearl/utils/man
 set PATH $PATH $PEARL_ROOT/mods/pearl/utils/bin
 source $PEARL_ROOT/mods/pearl/utils/lib/core.fish
 source $PEARL_ROOT/mods/pearl/utils/lib/aliases.sh
+source $PEARL_ROOT/mods/pearl/utils/lib/aliases.fish
 source $PEARL_ROOT/mods/pearl/utils/lib/utils.fish
 source $PEARL_ROOT/mods/pearl/utils/lib/prompt.fish
 source $PEARL_ROOT/mods/pearl/utils/lib/terminals.fish
